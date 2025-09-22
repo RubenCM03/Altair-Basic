935e3656d5f652a3c14d891eb1d9756949678a49
Ruben Cordero Muñoz <rco4577@proven.local>
2025-09-22 19:31:09 +0200

    Updates

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+st.header("Show your image")
+
 uploaded_file = st.file_uploader("Upload an image")
 if uploaded_file is not None:
-    with st.container(horizontal_alignment="center", ):
-        st.image(uploaded_file)
+    with st.container(horizontal_alignment="center"):
+        st.image(uploaded_file,  use_container_width=True)
